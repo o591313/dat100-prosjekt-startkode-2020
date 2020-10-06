5bef4cfc82acb43751a298888dcb7935ddfc92c7
o591313 <69842803+o591313@users.noreply.github.com>
2020-10-06 15:15:32 +0200

    Begynte på oppgave 1 -lyslo

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -5,73 +5,78 @@ import no.hvl.dat100ptc.TODO;
 public class GPSPoint {
 
 	// TODO - objektvariable
-		
+	// dobbeltsjekk variabeltypene -lyslo
+	private int time;
+	private double latitude;
+	private double longitude;
+	private double elevation;
+
+	// TODO - konstruktur
 	public GPSPoint(int time, double latitude, double longitude, double elevation) {
-
-		// TODO - konstruktur
-
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
-
+		time = 0;
+		latitude = 0;
+		longitude = 0;
+		elevation = 0;
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return time;
+
 	}
 
 	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
+
+		this.time = time;
 
 	}
 
 	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return latitude;
+
 	}
 
 	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.latitude = latitude;
+
 	}
 
 	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return longitude;
+
 	}
 
 	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.longitude = longitude;
+
 	}
 
 	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		return elevation;
+
 	}
 
 	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		this.elevation = elevation;
+
 	}
-	
+
 	public String toString() {
-		
+
 		String str;
-		
+
 		// TODO - start
 
 		throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - slutt
-		
+
 	}
 }
