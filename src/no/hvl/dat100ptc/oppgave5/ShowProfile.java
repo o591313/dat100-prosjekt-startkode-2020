a73be5fcd7afef51a37e01ce82298627aadf582a
o591313 <69842803+o591313@users.noreply.github.com>
2020-10-12 21:41:29 +0200

    fikser litt smått

--- src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
@@ -12,19 +12,19 @@ import javax.swing.JOptionPane;
 
 public class ShowProfile extends EasyGraphics {
 
-	private static final int MARGIN = 50;		// margin on the sides 
-	
+	private static final int MARGIN = 50; // margin on the sides
+
 	private static int MAXBARHEIGHT = 500; // assume no height above 500 meters
-	
+
 	private GPSPoint[] gpspoints;
 
 	public ShowProfile() {
 
 		String filename = JOptionPane.showInputDialog("GPS data filnavn: ");
-		GPSComputer gpscomputer =  new GPSComputer(filename);
+		GPSComputer gpscomputer = new GPSComputer(filename);
 
 		gpspoints = gpscomputer.getGPSPoints();
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -38,19 +38,29 @@ public class ShowProfile extends EasyGraphics {
 		makeWindow("Height profile", 2 * MARGIN + 3 * N, 2 * MARGIN + MAXBARHEIGHT);
 
 		// top margin + height of drawing area
-		showHeightProfile(MARGIN + MAXBARHEIGHT); 
+		showHeightProfile(MARGIN + MAXBARHEIGHT);
 	}
 
 	public void showHeightProfile(int ybase) {
 
 		// ybase indicates the position on the y-axis where the columns should start
-	
-		int x = MARGIN,y;
+
+		int x = MARGIN, y;
 
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
-	
+		setColor(0, 0, 255);
+		for (int i = 0; i < gpspoints.length; i++) {
+
+			y = (int) gpspoints[i].getElevation();
+
+			drawLine(MARGIN + i * 3, ybase, MARGIN + i * 3, ybase - y);
+
+			if (y <= 0) {
+				drawLine(MARGIN + i * 3, ybase, MARGIN + i * 3, 0);
+			}
+		}
+
 		// TODO - SLUTT
 	}
 
--- src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
@@ -16,7 +16,7 @@ public class ShowRoute extends EasyGraphics {
 
 	private GPSPoint[] gpspoints;
 	private GPSComputer gpscomputer;
-	
+
 	public ShowRoute() {
 
 		String filename = JOptionPane.showInputDialog("GPS data filnavn: ");
@@ -35,7 +35,7 @@ public class ShowRoute extends EasyGraphics {
 		makeWindow("Route", MAPXSIZE + 2 * MARGIN, MAPYSIZE + 2 * MARGIN);
 
 		showRouteMap(MARGIN + MAPYSIZE);
-		
+
 		showStatistics();
 	}
 
@@ -45,30 +45,35 @@ public class ShowRoute extends EasyGraphics {
 		double maxlon = GPSUtils.findMax(GPSUtils.getLongitudes(gpspoints));
 		double minlon = GPSUtils.findMin(GPSUtils.getLongitudes(gpspoints));
 
-		double xstep = MAPXSIZE / (Math.abs(maxlon - minlon)); 
+		double xstep = MAPXSIZE / (Math.abs(maxlon - minlon));
 
 		return xstep;
 	}
 
 	// antall y-pixels per breddegrad
 	public double ystep() {
-	
+
 		double ystep;
-		
+
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
+
+		double maxlat = GPSUtils.findMax(GPSUtils.getLatitudes(gpspoints));
+		double minlat = GPSUtils.findMin(GPSUtils.getLatitudes(gpspoints));
+
+		ystep = MAPYSIZE / (Math.abs(maxlat - minlat));
+
+		return ystep;
 
 		// TODO - SLUTT
-		
+
 	}
 
 	public void showRouteMap(int ybase) {
 
 		// TODO - START
-		
+
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 		// TODO - SLUTT
 	}
 
@@ -76,13 +81,13 @@ public class ShowRoute extends EasyGraphics {
 
 		int TEXTDISTANCE = 20;
 
-		setColor(0,0,0);
-		setFont("Courier",12);
-		
+		setColor(0, 0, 0);
+		setFont("Courier", 12);
+
 		// TODO - START
-		
+
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 		// TODO - SLUTT;
 	}
 
